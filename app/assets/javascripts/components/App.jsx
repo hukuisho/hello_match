f81bdeb1e93384ca0fda15367c11ee68c4bc9146
sho mukaiyama <shomukaiyama3@gmail.com>
2023-10-19 00:38:18 +0900

    環境構築

---
+++ app/assets/javascripts/components/App.jsx
@@ -0,0 +1,14 @@
+export default function App() {
+  return (
+    <React.Fragment>
+      <h1>Hello World</h1>
+    </React.Fragment>
+  );
+}
+export default function App2() {
+  return (
+    <React.Fragment>
+      <h1>Hello World!!!!!!!!!</h1>
+    </React.Fragment>
+  );
+}
