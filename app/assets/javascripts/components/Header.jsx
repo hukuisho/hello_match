56c2a5fe6a4fdf206c2ca50a41c2fbb9a6991f4a
sho mukaiyama <shomukaiyama3@gmail.com>
2023-10-19 20:48:53 +0900

    header

--- app/assets/javascripts/components/Header.jsx
+++ app/assets/javascripts/components/Header.jsx
@@ -1,7 +1,19 @@
-export default function Header() {
+// const mockHistoryPush = jest.fn();
+
+// jest.mock("react-router-dom", () => ({
+//   useHistory: () => ({
+//     push: mockHistoryPush, // pushメソッドをダミー関数で上書きする。
+//   }),
+// }));
+// import { render, screen, cleanup } from "@testing-library/react";
+// import userEvent from "@testing-library/user-event";
+function Header() {
   return (
-    <React.Fragment>
-      <h1>Hello World</h1>
-    </React.Fragment>
+    <div class="header">
+      <a href="/" class="header__title">
+        HelloMatch
+      </a>
+    </div>
   );
 }
+export default Header;
