54ad7202de9548f0a11c12000348f9cba2a0a407
sho mukaiyama <shomukaiyama3@gmail.com>
2023-10-19 20:16:45 +0900

    base

--- app/assets/javascripts/components/App.jsx
+++ app/assets/javascripts/components/App.jsx
@@ -1,14 +1,7 @@
-export default function App() {
-  return (
-    <React.Fragment>
-      <h1>Hello World</h1>
-    </React.Fragment>
-  );
-}
-export default function App2() {
-  return (
-    <React.Fragment>
-      <h1>Hello World!!!!!!!!!</h1>
-    </React.Fragment>
-  );
-}
+// export default function App() {
+//   return (
+//     <React.Fragment>
+//       <h1>Hello World</h1>
+//     </React.Fragment>
+//   );
+// }
---
+++ app/assets/javascripts/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <React.Fragment>
+      <h1>Hello World</h1>
+    </React.Fragment>
+  );
+}
